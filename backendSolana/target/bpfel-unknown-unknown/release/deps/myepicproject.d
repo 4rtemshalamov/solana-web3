7ad7bd5b5a5e257d77d42d3ef65b5c87df589c95
artemshalamov <iart1756@gmail.com>
2022-10-08 13:01:26 +0500

    Store structs on our program.

--- backendSolana/target/bpfel-unknown-unknown/release/deps/myepicproject.d
+++ backendSolana/target/bpfel-unknown-unknown/release/deps/myepicproject.d
@@ -1,7 +1,7 @@
-/Users/artemshalamov/myepicproject/target/bpfel-unknown-unknown/release/deps/myepicproject.so: programs/myepicproject/src/lib.rs
+/Users/artemshalamov/WebstormProjects/tutorials/gif-portal-starter/backendSolana/target/bpfel-unknown-unknown/release/deps/myepicproject.so: programs/myepicproject/src/lib.rs
 
-/Users/artemshalamov/myepicproject/target/bpfel-unknown-unknown/release/deps/libmyepicproject.rlib: programs/myepicproject/src/lib.rs
+/Users/artemshalamov/WebstormProjects/tutorials/gif-portal-starter/backendSolana/target/bpfel-unknown-unknown/release/deps/libmyepicproject.rlib: programs/myepicproject/src/lib.rs
 
-/Users/artemshalamov/myepicproject/target/bpfel-unknown-unknown/release/deps/myepicproject.d: programs/myepicproject/src/lib.rs
+/Users/artemshalamov/WebstormProjects/tutorials/gif-portal-starter/backendSolana/target/bpfel-unknown-unknown/release/deps/myepicproject.d: programs/myepicproject/src/lib.rs
 
 programs/myepicproject/src/lib.rs:
